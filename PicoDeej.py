835c3613c6d08a3c890c396594b1540d34bc87f6
Leo Durrant <poweredbykurisu@hotmail.com>
2025-03-02 20:44:45 +0000

    feat: updated script to support switches

--- PicoDeej.py
+++ PicoDeej.py
@@ -2,9 +2,15 @@ import machine
 import utime
 
 # Deej vars
-NUM_SLIDERS = 3
+NUM_SLIDERS = 2
+NUM_SWITCHES = 3
 
 analogSliderValues = [0, 0, 0]
+switchValues = [
+    machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP),
+    machine.Pin(3, machine.Pin.IN, machine.Pin.PULL_UP),
+    machine.Pin(4, machine.Pin.IN, machine.Pin.PULL_UP)
+]
 
 slideMaster = machine.ADC(26)
 slideProg1 = machine.ADC(27)
@@ -14,15 +20,14 @@ slideProg2 = machine.ADC(28)
 def updateSliderValues():
     for x in range(NUM_SLIDERS):
         if(x == 0):
-            analogSliderValues[0] = slideMaster.read_u16()
+            analogSliderValues[0] = slideMaster.read_u16() //64
         elif(x == 1):
-            analogSliderValues[1] = slideProg1.read_u16()
-        elif(x == 2):
-            analogSliderValues[2] = slideProg2.read_u16()
+            analogSliderValues[1] = slideProg1.read_u16() //64
 
 # Make a string with all slider values and print it
 def sendSliderValues():
     builtString = ""
+    builtString2 = ""
     
     for x in range(NUM_SLIDERS):
         builtString +=  str(analogSliderValues[x])
@@ -30,9 +35,14 @@ def sendSliderValues():
         if(x < NUM_SLIDERS - 1):
             builtString += '|'
     
-    print(builtString)
-
-
+    for z in range(NUM_SWITCHES):
+        builtString2 += str(1 - switchValues[z].value())
+        
+        if (z < NUM_SWITCHES - 1):
+            builtString2 += '|'
+    
+    print(f"{builtString} {builtString2}")
+  
 while(True):
     updateSliderValues()
     sendSliderValues()
